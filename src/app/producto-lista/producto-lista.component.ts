d7059182ded5b2c800567e2c35311b3ac27b1f49
Robinson Lopez <robins.616009@gmail.com>
2025-02-27 15:09:58 -0500

    Listado y clase de servicio

--- src/app/producto-lista/producto-lista.component.ts
+++ src/app/producto-lista/producto-lista.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Producto } from '../producto';
+import { ProductoService } from '../producto.service';
 
 @Component({
   selector: 'app-producto-lista',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   templateUrl: './producto-lista.component.html'
 })
 export class ProductoListaComponent {
+  productos: Producto[];
+    
+  constructor(private productoServicio: ProductoService){}
+
+  ngOnInit(){
+    //Cargamos los productos
+    this.obtenerProductos();
+  }
+
+  private obtenerProductos(){
+    //Consumir los datos del observable (suscribirnos)
+    this.productoServicio.obtenerProductosLista().subscribe(
+      (datos => {
+        this.productos = datos;
+      })
+    );
+  }
 
 }
